a410437fa68e3832bffa29097d5b8be2f04d18d4
Aurélie Nouaille-Degorce <aurelie@colom.be>
2019-06-03 10:57:58 +0200

    Concatenates two videos (bug: the first video is all black) and exports the result. Draft for adding overlay text layers.

---
+++ VideoWithOverlay/TextPartsForOverlay.swift
@@ -0,0 +1,25 @@
+//
+//  TextPartsForOverlay.swift
+//  VideoWithOverlay
+//
+//  Created by Aurélie Nouaille-Degorce on 02/06/2019.
+//  Copyright © 2019 Nouaille-Degorce. All rights reserved.
+//
+
+import Foundation
+
+struct TextPartsForOverlay {
+    
+    // Name of the person, will appear at the top of the overlay
+    var personName: String = "MATHILDE"
+    
+    // First part of the text that will appear after seeing bars, between the bars
+    var partOne: String = "UX / UI"
+    
+    // Second part of the text, will appear just under the bars
+    var partTwo: String = "DESIGNER"
+    
+    // Workplace name will appear at the end of the overlay
+    var workPlace: String = "FABERNOVEL"
+    
+}
